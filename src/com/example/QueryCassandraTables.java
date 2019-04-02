982947ea5c7a29f420623b47d2486c4e9b85dad9
Abul Basar <abulbasar@gmail.com>
2019-04-02 13:10:12 +0530

    update

--- src/com/example/QueryCassandraTables.java
+++ src/com/example/QueryCassandraTables.java
@@ -5,6 +5,8 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.api.java.UDF1;
+import org.apache.spark.sql.types.DataTypes;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -41,18 +43,28 @@ public class QueryCassandraTables {
 		
 		spark = SparkSession.builder().config(conf).getOrCreate();
 
+		spark.udf().register("toMyUpperCase", new UDF1<String, String>() {
+            public String call(String value){
+                return value.toUpperCase();
+            }
+
+        }, DataTypes.StringType);
+
 
 		
 		registerView("demo", "movies");
 		registerView("demo", "ratings");
 		
-		Dataset<Row> df = spark.sql("select t1.movieid, t1.title, avg(t2.rating) avg_rating"
+		Dataset<Row> df = spark.sql("select t1.movieid, toMyUpperCase(t1.title), avg(t2.rating) avg_rating"
 				+ " from movies t1 join ratings t2 on t1.movieId = t2.movieid " 
-				+ "group by t1.movieid, t1.title having count(1) > 100 order by avg_rating desc");
+				+ "group by t1.movieid, t1.title having count(1) > 100 order by avg_rating desc limit 10");
 		
 		df.show();
 		
-		df.coalesce(1).write().format("csv").mode(SaveMode.Overwrite).save("/tmp/movie-rating");
+		df.coalesce(1).write()
+                .format("csv")
+                .mode(SaveMode.Overwrite)
+                .save("/tmp/top10movies");
 
 	}
 
